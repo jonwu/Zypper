9373b49ea620f2349d087f01de0b0fe9222a7f20
haoguan <haohowardguan@gmail.com>
2015-03-24 00:23:13 -0700

    removed uuid

--- app/src/js/components/Question.js
+++ app/src/js/components/Question.js
@@ -2,20 +2,6 @@ var React = require('react');
 var Textarea = require('react-textarea-autosize');
 var Question = React.createClass({
 
-	uuid: function () {
-    var i, random;
-    var uuid = '';
-    for (i = 0; i < 32; i++) {
-        random = Math.random() * 16 | 0;
-        if (i === 8 || i === 12 || i === 16 || i === 20) {
-            uuid += '-';
-        }
-
-        uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
-    }
-    return uuid;
-	},
-
 	handleOnKeyUp: function(i){
 
 		var self = this;
